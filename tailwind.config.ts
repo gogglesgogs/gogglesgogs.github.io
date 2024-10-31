44709692ca814d45fd8127eced14d15dd3cdb87d
gogglesgogs <ngkhoa0075@gmail.com>
2024-11-01 01:17:46 +0700

    Integrate React Router

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,16 @@
 import { type Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [typography],
 } satisfies Config;
