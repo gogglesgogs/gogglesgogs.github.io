7daf7319a6cc82bbabc349d745fe2feaa70b1e6f
Goggles <82652626+gogglesgogs@users.noreply.github.com>
2024-11-01 16:29:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          tailwind: ['tailwind-merge', 'clsx'],
+          animation: ['framer-motion'],
+        },
+        compact: true
+      }
+    }
+  },
 });
