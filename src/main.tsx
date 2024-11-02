bdf3f84de86a909a22bfb925a5428e529f778180
gogglesgogs <ngkhoa0075@gmail.com>
2024-11-03 00:53:55 +0700

    redo code splitting

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
 import './index.css';
 
-const App = lazy(() => import('./App.tsx'));
 const Nav = lazy(() => import('./components/Nav.tsx'));
 const Footer = lazy(() => import('./components/Footer.tsx'));
 
